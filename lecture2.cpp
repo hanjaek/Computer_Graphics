f0717d7e5c9d280ded7005aed94bfed330f0f6ad
Kimhanjae7 <hjk2512@naver.com>
2025-03-20 13:57:08 +0900

    Add lecture2 description

--- lecture2.cpp
+++ lecture2.cpp
@@ -9,10 +9,17 @@ void RenderScene(void){
     // 가장 최근에 그려진 이미지를 지움: Buffer...
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glColor3f(1.0f, 0.0f, 0.0f);
+    // 드로잉 색상 지정
+    glColor3f(1.0f, 1.0f, 1.0f);
 
-    // 왼아래, 오른위 점
-    // 좌표계 설정이 없다면, 일반적으로 직교 투영으로 [-1,1] 범위 내에서 작동.
+    /**
+    - 사각형 그려주는 함수 (왼 위x, 왼 위y, 오른 아래x, 오른 아래 y)
+
+    - 왼아래, 오른위 점
+
+    - 좌표계 설정이 없다면, 일반적으로 직교 투영으로 [-1,1] 범위 내에서 작동 
+      -> 변경하려면 glOrtho() 함수 사용
+    **/
     glRectf(-0.5f, -0.5f, 0.5f, 0.5f);
 
     // 드로잉 명령을 전달
@@ -21,13 +28,13 @@ void RenderScene(void){
 
 void SetupRC(void){
     cout << "SetupRC" << endl;
-    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 }
 
 int main(int argc, char** argv){
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(640, 480);
+    glutInitWindowSize(640, 640);
     glutInitWindowPosition(400, 400);
     glutCreateWindow("lecture2_2_1");
 
