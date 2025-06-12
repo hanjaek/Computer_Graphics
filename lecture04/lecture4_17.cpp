a92e3df362f8547d001c4f9de5f0cea58f52f1f4
hanjaek <hjk2512@naver.com>
2025-06-12 23:30:22 +0900

    Update lecture4 - Draw Atom

--- lecture04/lecture4_17.cpp
+++ lecture04/lecture4_17.cpp
@@ -7,6 +7,7 @@ static float yRot = 0.0f;
 
 void RenderScene(void)
 {
+
     //핵 주위의 공전 각도 설정
     static GLfloat fElect1 = 0.0f;
     //창의 내용을 지움
@@ -15,7 +16,7 @@ void RenderScene(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    glTranslatef(0.0f, 0.0f, -50.0f);
+    glTranslatef(0.0f, 0.0f, -200.0f);
     //적색 원자핵, glColor3ub : 0-255로 스케일링 해서 이용(unsigned char)
     glColor3ub(255, 0, 0);
     glutSolidSphere(20.0f, 15, 15);
@@ -27,7 +28,7 @@ void RenderScene(void)
     //공전각 에 따 라 회전시킨다.
     glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
     //원점에서 궤도 거리만큼 이동하는 변환
-    glTranslatef(30.0f, 0.0f, 0.0f);
+    glTranslatef(90.0f, 0.0f, 0.0f);
     //전자를 그 린다.
     glutSolidSphere(6.0f, 15, 15);
     //관측 변환의 내 용을 복구 한다 .
@@ -37,7 +38,7 @@ void RenderScene(void)
     glColor3ub(0, 255, 0);
     glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
     glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-    glTranslatef(-50.0f, 0.0f, 0.0f);
+    glTranslatef(-70.0f, 0.0f, 0.0f);
     glutSolidSphere(6.0f, 15, 15);
     glPopMatrix();
     //세 번째 전자의 궤도
@@ -45,7 +46,7 @@ void RenderScene(void)
     glColor3ub(0, 255, 255);
     glRotatef(-45.0f, 0.0f, 0.0f, 1.0f);
     glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-    glTranslatef(0.0f, 0.0f, 50.0f);
+    glTranslatef(0.0f, 0.0f, 60.0f);
     glutSolidSphere(6.0f, 15, 15);
     glPopMatrix();
     //공전각을 증가시 킨다 .
@@ -83,27 +84,22 @@ void TimerFunction(int value)
 
 void ChangeSize(GLsizei w, GLsizei h)
 {
-    GLdouble nRange = 100.0f;
-    GLdouble aspectRatio;
+    GLfloat fAspect;
 
-    // O으로 나누지 않도록 함
     if (h == 0)
         h = 1;
 
     glViewport(0, 0, w, h);
 
+    fAspect = (GLfloat)w / (GLfloat)h;
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
+    gluPerspective(80.0f, fAspect, 1.0f, 500.0f);
 
-    // 클리핑 영역 설정(left, right, bottom, top, near, far 순서)
-    aspectRatio = (GLfloat)w / (GLfloat)h;
-    if (w <= h)
-        glOrtho(-nRange, nRange, -nRange / aspectRatio, nRange / aspectRatio, -nRange, nRange);
-    else
-        glOrtho(-nRange * aspectRatio, nRange * aspectRatio, -nRange, nRange, -nRange, nRange);
-
-    glMatrixMode(GL_MODELVIEW);  //GL_MODELVIEW : 모델(그리는 물체)에 대한 각각의 좌표계
-    glLoadIdentity();   //좌표계 초기화(원점)
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    //glTranslatef(0.0f, 0.0f, -0.0f);
 }
 
 int main(int argc, char* argv[])
@@ -115,7 +111,7 @@ int main(int argc, char* argv[])
 
     glutDisplayFunc(RenderScene);
     glutReshapeFunc(ChangeSize);
-   glutTimerFunc(33, TimerFunction, 1);
+   // glutTimerFunc(33, TimerFunction, 1);
 
     SetupRC();
 
